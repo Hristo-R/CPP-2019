d0fc0f0119ef6265d652e0b4ca2a05ff073ed938
Hristo-R <hristo.razpopov@abv.bg>
2019-11-15 00:17:10 +0200

    Edited PrintTheASCIITable/main.cpp

--- PrintTheASCIITable/main.cpp
+++ PrintTheASCIITable/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main() {
         cout << "ASCII Table\n";
         cout << "decimal\tchar\n";
+		
         for (int i = 0; i < 255; i++) {
             char DecimalToChar = (char)i;
             cout << i << "\t" << DecimalToChar << endl;
