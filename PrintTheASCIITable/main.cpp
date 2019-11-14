14d6e5523289328f53952a5e33f0fba03513b3f2
Hristo-R <hristo.razpopov@abv.bg>
2019-11-15 00:19:23 +0200

    Edited PrintTheASCIITable/main.cpp

--- PrintTheASCIITable/main.cpp
+++ PrintTheASCIITable/main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-        cout << "ASCII Table\n";
+        cout << "The ASCII Table\n";
         cout << "decimal\tchar\n";
 		
         for (int i = 0; i < 255; i++) {
