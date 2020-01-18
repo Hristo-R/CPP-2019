37d7bc4ad7d26be5a3583c02a6fbb90b879dc026
Hristo-R <hristo.razpopov@abv.bg>
2020-01-18 14:50:00 +0200

    Added SortingArray/main.cpp

---
+++ CPP-Basics/SortingArray/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+void sortingArrayAscending(int * arr, int sizeArr)
+{
+    int temp;
+    for (int i = 0; i < sizeArr; i++) {
+        for (int j = i + 1; j < sizeArr; j++) {
+            if (arr[i] > arr[j]) {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+void printingArray(int * arr, int sizeArr)
+{
+    for (int i = 0; i < sizeArr; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int n;
+    cout << "Input array size: " << endl;
+    cin >> n;
+
+    int arr[n];
+    cout << "Input elements: " << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    sortingArrayAscending(arr, n);
+    cout << "Sorted array as ascending order:" << endl;
+    printingArray(arr, n);
+
+    return 0;
+}
