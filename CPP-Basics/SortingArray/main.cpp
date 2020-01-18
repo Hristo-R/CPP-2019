c6c00f753543e4f1340d79992b8b475080bc3ac3
Hristo-R <hristo.razpopov@abv.bg>
2020-01-18 15:12:13 +0200

    Added descending sorting in Sorting2DArray/main.cpp

--- CPP-Basics/SortingArray/main.cpp
+++ CPP-Basics/SortingArray/main.cpp
@@ -16,6 +16,20 @@ void sortingArrayAscending(int * arr, int sizeArr)
     }
 }
 
+void sortingArrayDescending(int * arr, int sizeArr)
+{
+    int temp;
+    for (int i = 0; i < sizeArr; i++) {
+        for (int j = i + 1; j < sizeArr; j++) {
+            if (arr[i] < arr[j]) {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
 void printingArray(int * arr, int sizeArr)
 {
     for (int i = 0; i < sizeArr; i++) {
@@ -38,6 +52,10 @@ int main()
 
     sortingArrayAscending(arr, n);
     cout << "Sorted array as ascending order:" << endl;
+    printingArray(arr, n);
+
+    sortingArrayDescending(arr, n);
+    cout << "Sorted array as descending order:" << endl;
     printingArray(arr, n);
 
     return 0;
