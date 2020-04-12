e04a3d01e9b076e631ff534150a7eaeda8ab3974
Hristo-R <hristo.razpopov@abv.bg>
2020-04-12 15:29:10 +0300

    Added 02.MemoryManagement-HomeworkExercises/Task2-RemoveNegativId/RemoveInvalid.h

---
+++ CPP-Advanced/02.MemoryManagement-HomeworkExercises/Task2-RemoveNegativId/RemoveInvalid.h
@@ -0,0 +1,19 @@
+#ifndef REMOVEINVALID_H_INCLUDED
+#define REMOVEINVALID_H_INCLUDED
+
+#include <list>
+
+
+void removeInvalid(std::list<Company*> &companies) {
+    for (std::list<Company*>::iterator itr = companies.begin(); itr != companies.end();) {
+        if ((*itr)->getId() < 0) {
+            itr = companies.erase(itr);
+        }
+        else {
+            ++itr;
+        }
+    }
+}
+
+
+#endif // REMOVEINVALID_H_INCLUDED
