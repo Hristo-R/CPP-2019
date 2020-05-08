23211d8dced503e3a785e55112933fdeb1e35800
Hristo R <hristo.razpopov@abv.bg>
2020-05-08 22:24:26 +0300

    Update main.cpp

--- CPP-Advanced/ArrayInFunction/main.cpp
+++ CPP-Advanced/ArrayInFunction/main.cpp
@@ -2,9 +2,9 @@
 
 using namespace std;
 
-void ChangeArray (double arr[][5]){
+void ChangeArray (double arr[][5]) {
     for(size_t row = 0; row < 3; row++) {
-        for(size_t col= 0; col< 5; col++){
+        for(size_t col= 0; col< 5; col++) {
             arr[row][col]++;
         }
     }
